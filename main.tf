66f09efb600c6b6c1434d0ac64da0182213cad9d
msatish1904 <99471611+msatish1904@users.noreply.github.com>
2024-02-07 14:20:54 +0530

    Update main.tf
    TEST

--- main.tf
+++ main.tf
@@ -4,18 +4,6 @@ provider "aws" {
     secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
 }
-
-terraform {
-  required_version = "<= 1.3.14" #Forcing which version of Terraform needs to be used
-  required_providers {
-    aws = {
-      version = "<= 5.0.0" #Forcing which version of plugin needs to be used.
-      source = "hashicorp/aws"
-    }
-  }
-}
-
-
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true
