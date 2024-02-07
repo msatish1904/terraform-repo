917fbcdd00bfa6d0dd10ddea50b2e4b43477fd89
msatish1904 <99471611+msatish1904@users.noreply.github.com>
2024-02-07 14:19:54 +0530

    Update variables.tf
    TEST

--- variables.tf
+++ variables.tf
@@ -12,10 +12,17 @@ variable "vpc_name" {}
 variable "IGW_name" {}
 variable "key_name" {}
 variable "public_subnet1_cidr" {}
+variable "public_subnet2_cidr" {}
+variable "public_subnet3_cidr" {}
+variable "private_subnet_cidr" {}
+variable "public_subnet1_name" {}
+variable "public_subnet2_name" {}
+variable "public_subnet3_name" {}
+variable "private_subnet_name" {}
 variable Main_Routing_Table {}
 variable "azs" {
   description = "Run the EC2 Instances in these Availability Zones"
-  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default = ["us-east-1a"]
 }
 variable "environment" { default = "dev" }
 variable "instance_type" {
